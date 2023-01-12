b5475728677e48508a39bd1183ade8745149cf57
Karina Babakova <taytynovak@mail.ru>
2023-01-12 18:11:01 +0700

    Решение задачи

--- task.py
+++ task.py
@@ -1,5 +1,13 @@
 #Написать программу, которая из имеющегося массива строк, формирует массив из строк, длинна которых меньше или равна 3 символа. 
 
-numbers = []
-for i in range (1,6):
-    numbers.append(int(input()))
+def form_array():
+    array = ["Hello", "2", "world", ":-)"]
+    size = len(array)
+    array_new = []
+    index = 0
+    for index in range (0,size):
+        if len(array[index]) <= 3:
+            array_new.append(array[index])
+    index += 1
+    print (array_new)
+form_array()
