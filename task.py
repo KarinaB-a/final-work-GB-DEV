22459c9bdd3cbb07d69b9473ab0734c534feacd4
Karina Babakova <taytynovak@mail.ru>
2023-01-12 18:23:30 +0700

    Решение с вводом массива с клавиатуры

--- task.py
+++ task.py
@@ -1,13 +1,16 @@
 #Написать программу, которая из имеющегося массива строк, формирует массив из строк, длинна которых меньше или равна 3 символа. 
 
+def input_array():
+    array = input(f'Введите элементы массива через запятые\n').split(',')
+    return array
+
 def form_array():
-    array = ["Hello", "2", "world", ":-)"]
+    array = input_array()
     size = len(array)
     array_new = []
-    index = 0
     for index in range (0,size):
         if len(array[index]) <= 3:
             array_new.append(array[index])
-    index += 1
     print (array_new)
 form_array()
+
